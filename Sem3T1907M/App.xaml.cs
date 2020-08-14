b0a5c25ce0df8894a850ba5e216cdcff2b751b25
Mtt-linh <63852165+Mtt-linh@users.noreply.github.com>
2020-08-14 10:58:48 +0700

    fakenews

--- Sem3T1907M/App.xaml.cs
+++ Sem3T1907M/App.xaml.cs
@@ -66,7 +66,7 @@ namespace Sem3T1907M
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(UWP1), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
